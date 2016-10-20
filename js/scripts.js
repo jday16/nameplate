f823c3a00a6e396b3d6cbab5dae1d726773bbf4e
jday16 <janessaday@eagles.ewu.edu>
2016-10-19 23:13:17 -0700

    added backstretch img

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 // Intialize Lettering JS
-$('h1').lettering();
+$("h1").lettering();
 //make backstrech go//
 
-$.backstretch('img/castle.jpg');
+$.backstretch("img/castle.jpg");
+
+$(".grid").masonry({/*options*/
+    itemSelector: '.grid-item",
+    columnWidth: 250,
+    gutter:24,
+    transitionDuration: '1s'})
