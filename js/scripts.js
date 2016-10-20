33a695e2e2a5a23ad7aa486ff053456b5e9975a8
jday16 <janessaday@eagles.ewu.edu>
2016-10-19 23:14:14 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,8 @@ $("h1").lettering();
 //make backstrech go//
 
 $.backstretch("img/castle.jpg");
-
-$(".grid").masonry({/*options*/
-    itemSelector: '.grid-item",
+$('.grid').masonry({/*options*/
+    itemSelector: '.grid-item',
     columnWidth: 250,
-    gutter:24,
-    transitionDuration: '1s'})
+    gutter: 24,
+    transitionDuration: '1s'});
